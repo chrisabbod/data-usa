975e973e485a6d31fd476a580637b6ae52f1f673
chris <chrisabbod@gmail.com>
2022-08-03 16:12:32 -0400

    create historical statistics fragment, viewmodel, and layout

---
+++ app/src/main/java/com/chrisabbod/data_usa/historical_statistics/HistoricalStatisticsViewModel.kt
@@ -0,0 +1,7 @@
+package com.chrisabbod.data_usa.historical_statistics
+
+import androidx.lifecycle.ViewModel
+
+class HistoricalStatisticsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
