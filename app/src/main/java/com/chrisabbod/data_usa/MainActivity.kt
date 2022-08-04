306b1538e4235d554a86ad0ac5de5fef152ef4f8
chris <chrisabbod@gmail.com>
2022-08-04 11:53:36 -0400

    Add nav_graph_main and can now navigate between local, state, and historical views

--- app/src/main/java/com/chrisabbod/data_usa/MainActivity.kt
+++ app/src/main/java/com/chrisabbod/data_usa/MainActivity.kt
@@ -2,15 +2,34 @@ package com.chrisabbod.data_usa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.chrisabbod.data_usa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val rootDestinations =
+        setOf(R.id.navigation_local, R.id.navigation_state, R.id.navigation_historical)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bottomNav = binding.bottomNav
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        val appBarConfiguration = AppBarConfiguration(
+            rootDestinations
+        )
+
+        bottomNav.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
