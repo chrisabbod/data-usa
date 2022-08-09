e0f7a7d29df4ac48adcc2feca5a172fb82843fcd
chris <chrisabbod@gmail.com>
2022-08-08 21:28:33 -0400

    Refactor HistoricalStatisticsFragment to remove usused code

--- app/src/main/java/com/chrisabbod/data_usa/historical_statistics/HistoricalStatisticsFragment.kt
+++ app/src/main/java/com/chrisabbod/data_usa/historical_statistics/HistoricalStatisticsFragment.kt
@@ -1,6 +1,5 @@
 package com.chrisabbod.data_usa.historical_statistics
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,23 +9,11 @@ import com.chrisabbod.data_usa.R
 
 class HistoricalStatisticsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HistoricalStatisticsFragment()
-    }
-
-    private lateinit var viewModel: HistoricalStatisticsFragmentViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_historical_statistics, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HistoricalStatisticsFragmentViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
