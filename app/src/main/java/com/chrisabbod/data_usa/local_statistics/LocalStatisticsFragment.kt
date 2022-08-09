c78237527d4b8c2cee145464de7935f0714bdc6e
chris <chrisabbod@gmail.com>
2022-08-08 21:12:31 -0400

    add dependency required for creating a shared ViewHolder using activityViewModels() and remove refactor LocalStatisticsFragment

--- app/src/main/java/com/chrisabbod/data_usa/local_statistics/LocalStatisticsFragment.kt
+++ app/src/main/java/com/chrisabbod/data_usa/local_statistics/LocalStatisticsFragment.kt
@@ -1,6 +1,5 @@
 package com.chrisabbod.data_usa.local_statistics
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,23 +9,11 @@ import com.chrisabbod.data_usa.R
 
 class LocalStatisticsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = LocalStatisticsFragment()
-    }
-
-    private lateinit var viewModel: LocalStatisticsFragmentViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_local_statistics, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LocalStatisticsFragmentViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
