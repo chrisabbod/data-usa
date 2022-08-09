88f443a1e19e201ee5b8d7a26da7054bdb417dc6
chris <chrisabbod@gmail.com>
2022-08-08 21:27:00 -0400

    Refactor StateStatisticsFragment to remove unused code

--- app/src/main/java/com/chrisabbod/data_usa/state_statistics/StateStatisticsFragment.kt
+++ app/src/main/java/com/chrisabbod/data_usa/state_statistics/StateStatisticsFragment.kt
@@ -1,6 +1,5 @@
 package com.chrisabbod.data_usa.state_statistics
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,23 +9,11 @@ import com.chrisabbod.data_usa.R
 
 class StateStatisticsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = StateStatisticsFragment()
-    }
-
-    private lateinit var viewModel: StateStatisticsFragmentViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_state_statistics, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(StateStatisticsFragmentViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
