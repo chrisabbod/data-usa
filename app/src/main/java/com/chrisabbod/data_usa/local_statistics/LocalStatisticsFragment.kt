5b14a62516e7fa5b17c52eaf415ef0fc053d7bdb
chris <chrisabbod@gmail.com>
2022-08-10 14:26:06 -0400

    Wire up adapter and provide it with dummy data

--- app/src/main/java/com/chrisabbod/data_usa/local_statistics/LocalStatisticsFragment.kt
+++ app/src/main/java/com/chrisabbod/data_usa/local_statistics/LocalStatisticsFragment.kt
@@ -5,17 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.chrisabbod.data_usa.databinding.FragmentLocalStatisticsBinding
+import com.chrisabbod.data_usa.model.CityData
 
 class LocalStatisticsFragment : Fragment() {
 
     private var binding: FragmentLocalStatisticsBinding? = null
+
+    private var cityDataList = listOf(
+        CityData("Macomb Township", 90758),
+        CityData("Shelby Township", 93757),
+        CityData("Sterling Heights", 35732),
+        CityData("Clinton Township", 23435),
+        CityData("Madison Heights", 54323),
+        CityData("Warren", 22335),
+        CityData("Detroit", 1423523),
+        CityData("Mount Clemens", 23521),
+        CityData("Royal Oak", 66432),
+        CityData("Romeo Township", 73245),
+        CityData("Washington Township", 23753),
+        CityData("Rochester", 53275)
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLocalStatisticsBinding.inflate(inflater, container, false)
+        binding?.rvLocalStatistics?.layoutManager = LinearLayoutManager(activity)
+        binding?.rvLocalStatistics?.adapter = LocalStatisticsAdapter(cityDataList)
 
         return binding!!.root
     }
@@ -23,13 +43,11 @@ class LocalStatisticsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
     }
 
     override fun onDestroy() {
         super.onDestroy()
         binding = null
     }
-
 }
 
