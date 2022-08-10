cd2fa341b087af33b28caed78aaead467274ef48
chris <chrisabbod@gmail.com>
2022-08-10 09:16:26 -0400

    Add viewbinding to fragment

--- app/src/main/java/com/chrisabbod/data_usa/local_statistics/LocalStatisticsFragment.kt
+++ app/src/main/java/com/chrisabbod/data_usa/local_statistics/LocalStatisticsFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.chrisabbod.data_usa.R
+import com.chrisabbod.data_usa.databinding.FragmentLocalStatisticsBinding
 
 class LocalStatisticsFragment : Fragment() {
+
+    private var binding: FragmentLocalStatisticsBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_local_statistics, container, false)
+        binding = FragmentLocalStatisticsBinding.inflate(inflater, container, false)
+
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 
 }
+
