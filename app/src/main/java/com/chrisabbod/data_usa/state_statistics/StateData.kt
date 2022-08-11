3652a07fa1cf4ded0a43aa1acb6d6b530a28c8ba
chris <chrisabbod@gmail.com>
2022-08-11 12:39:19 -0400

    Update StateData fields to use val rather than var

--- app/src/main/java/com/chrisabbod/data_usa/state_statistics/StateData.kt
+++ app/src/main/java/com/chrisabbod/data_usa/state_statistics/StateData.kt
@@ -1,6 +1,6 @@
 package com.chrisabbod.data_usa.state_statistics
 
 data class StateData(
-    var stateName: String,
-    var statePopulation: Int,
+    val stateName: String,
+    val statePopulation: Int,
 )
