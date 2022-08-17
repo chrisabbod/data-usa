b50912889d3e67426042fed2870f071c4c35e253
chris <chrisabbod@gmail.com>
2022-08-16 23:35:16 -0400

    Create title with multiple different colors by utilizing a span

--- app/src/main/java/com/chrisabbod/data_usa/LoginActivity.kt
+++ app/src/main/java/com/chrisabbod/data_usa/LoginActivity.kt
@@ -3,6 +3,11 @@ package com.chrisabbod.data_usa
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.text.toSpannable
 import com.chrisabbod.data_usa.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -14,6 +19,8 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setTitleColor()
+
         binding.btnLogin.setOnClickListener {
             startActivity(
                 Intent(
@@ -22,5 +29,29 @@ class LoginActivity : AppCompatActivity() {
                 )
             )
         }
+    }
+
+    private fun setTitleColor() {
+        val span: Spannable = resources.getString(R.string.app_name).toSpannable()
+
+        span.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(this, R.color.red)),
+            5,
+            6,
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+        span.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(this, R.color.gray)),
+            6,
+            7,
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+        span.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(this, R.color.blue)),
+            7,
+            8,
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+        binding.tvLoginTitle.text = span
     }
 }
